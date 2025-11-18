bb56977055ca69ac8beeaeacac4fd96f1f888ef8
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2025-11-18 14:03:30 +0700

    ready for production: refactor entire function

--- src/enrich.py
+++ src/enrich.py
@@ -2,43 +2,47 @@
 ==================================================================
 BUDGET ENRICHMENT MODULE
 ------------------------------------------------------------------
-This module enriches raw or staging-level budget data with 
-standardized metadata, mappings, and business logic to produce 
-a clean, analysis-ready mart for downstream usage.
+This module is responsible for transforming raw Budget Allocation 
+data with standardized metadata, mappings, business logic to 
+produce a clean and analysis-ready mart for downstream usage.
 
-It encapsulates data cleaning, normalization, and mapping steps 
-to ensure consistent keys across platforms (e.g. chuong_trinh, 
-noi_dung, ma_ngan_sach_cap_1), allowing seamless joins with 
-marketing performance data (e.g. Facebook, Google, TikTok).
+By centralizing enrichment rules, this module ensures transparency, 
+consistency, and maintainability across the marketing data pipeline 
+to build insight-ready tables.
 
-✔️ Merges raw/staging budget data with dimension tables for unified identifiers  
-✔️ Applies naming conventions, formats fields, and parses month/year info  
-✔️ Adds audit columns like `is_deleted`, `created_at`, `source` for traceability  
-✔️ Returns a fully structured DataFrame ready for loading into the budget mart
+✔️ Merges budget data with dimension tables for unified identifiers  
+✔️ Standardizes program track, type and group naming conventions  
+✔️ Extracts and normalizes key performance metrics across campaigns  
+✔️ Cleans and validates data to ensure schema and field consistency  
+✔️ Reduces payload size by removing redundant or raw field(s)
 
-⚠️ This module does not fetch or write data from/to external systems.  
-It expects staging data as input and returns enriched data for downstream consumption.
+⚠️ This module focuses *only* on enrichment and transformation logic.  
+It does **not** handle data fetching, ingestion or staging
 ==================================================================
 """
+
 # Add root directory to sys.path for absolute imports of internal modules
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 
-# Add datetime utilities for integration
+# Add Python datetime utilities for integration
 from datetime import datetime
 
-# Add logging ultilities for integration
+# Add Python logging ultilities for integraton
 import logging
-
-# Add timezone ultilites for integration
-import pytz
 
 # Add Python Pandas libraries for integration
 import pandas as pd
 
-# Add Python "re" library for handling
+# Add Python timezone ultilities for integration
+import pytz
+
+# Add Python "re" libraries for integraton
 import re
+
+# Add Python time ultilities for integration
+import time
 
 # Get environment variable for Company
 COMPANY = os.getenv("COMPANY") 
@@ -61,86 +65,147 @@ LAYER = os.getenv("LAYER")
 # Get environment variable for Mode
 MODE = os.getenv("MODE")
 
-# 1. ENRICH BUDGET ALLOCATION FROM INGESTION PHASE
+# 1. ENRICH BUDGET ALLOCATION FROM STAGING PHASE
 
-# 1.1. Enrich budget allocation insights
-def enrich_budget_insights(df: pd.DataFrame) -> pd.DataFrame:
-    print("🚀 [ENRICH] Starting to enrich raw budget data...")
-    logging.info("🚀 [ENRICH] Starting to enrich raw budget data...")
-    
-    if df.empty:
-        print("⚠️ [ENRICH] Budget input Python DataFrame is empty then enrichment is skipped.")
-        logging.warning("⚠️ [ENRICH] Budget input Python DataFrame is empty then enrichment is skipped.")
-        return df
-    
-    # 1.1.1. Add ingestion timestamp with last_update_at
+# 1.1. Enrich budget fields from staging phase
+def enrich_budget_fields(enrich_df_input: pd.DataFrame, enrich_table_id: str) -> pd.DataFrame:
+    print(f"🚀 [ENRICH] Starting to enrich staging Budget Allocation for {len(enrich_df_input)} row(s)...")
+    logging.info(f"🚀 [ENRICH] Starting to enrich staging Budget Allocation for {len(enrich_df_input)} row(s)...")
+
+    # 1.1.1. Start timing the staging Budget Allocation enrichment
+    enrich_time_start = time.time()   
+    enrich_sections_status = {}
+    enrich_sections_time = {}
+    enrich_df_table = pd.DataFrame()
+    enrich_df_budget = pd.DataFrame()
+    enrich_df_other = pd.DataFrame()
+    print(f"🔍 [ENRICH] Proceeding to enrich staging Budget Allocation for {len(enrich_df_input)} row(s) at {time.strftime('%Y-%m-%d %H:%M:%S')}...")
+    logging.info(f"🔍 [ENRICH] Proceeding to enrich staging Budget Allocation for {len(enrich_df_input)} row(s) at {time.strftime('%Y-%m-%d %H:%M:%S')}...")
+
+    # 1.1.2. Validate input for the staging Budget Allocation enrichment
+    enrich_section_name = "[ENRICH] Validate input for the staging Budget Allocation enrichment"
+    enrich_section_start = time.time()    
     try:
-        print(f"🔄 [ENRICH] Adding budget ingestion timestamp for {len(df)} row(s) with last_update_at...")
-        logging.info(f"🔄 [ENRICH] Adding budget ingestion timestamp for {len(df)} row(s) with last_update_at...")
-        df["last_updated_at"] = datetime.utcnow().replace(tzinfo=pytz.UTC)
-        print(f"✅ [ENRICH] Successfully added budget ingestion timestamp for {len(df)} row(s) with last_update_at.")
-        logging.info(f"✅ [ENRICH] Successfully added budget ingestion timestamp for {len(df)} row(s) with last_update_at.")
-    except Exception as e:
-        print(f"❌ [ENRICH] Failed to add budget ingestion timestamp due to {e}.")
-        logging.exception("❌ [ENRICH] Failed to add budget ingestion timestamp due to {e}.")
-    return df
+        if enrich_df_input.empty:
+            enrich_sections_status[enrich_section_name] = "failed"
+            print("⚠️ [ENRICH] Empty staging Budget Allocation provided then enrichment is suspended.")
+            logging.warning("⚠️ [ENRICH] Empty staging Budget Allocation provided then enrichment is suspended.")
+        else:
+            enrich_sections_status[enrich_section_name] = "succeed"
+            print("✅ [ENRICH] Successfully validated input for staging Budget Allocation enrichment.")
+            logging.info("✅ [ENRICH] Successfully validated input for staging Budget Allocation enrichment.")
+    finally:
+        enrich_sections_time[enrich_section_name] = round(time.time() - enrich_section_start, 2)
 
-# 2. ENRICH BUDGET FIELDS FROM STAGING PHASE
-
-# 2.1. Enrich budget fields by adding derived fields
-def enrich_budget_fields(df: pd.DataFrame, table_id: str) -> pd.DataFrame:
-    print("🚀 [ENRICH] Starting to enrich budget staging data...")
-    logging.info("🚀 [ENRICH] Starting to enrich budget staging data...")    
-
-    if df.empty:
-        print("⚠️ [ENRICH] Budget input Python DataFrame is empty then enrichment is skipped.")
-        logging.warning("⚠️ [ENRICH] Budget input Python DataFrame is empty then enrichment is skipped.")
-        return df
-
-    # 2.1.1. Standardize platform classification
-    if "nen_tang" in df.columns:
-        print("🔍 [ENRICH] Standardizing budget platform classification by 'nen_tang'...")
-        logging.info("🔍 [ENRICH] Standardizing budget platform classification by 'nen_tang'...")          
-        df["nen_tang"] = df["nen_tang"].astype(str).str.strip().str.lower()
-        print("✅ [ENRICH] Successfully standardized budget platform classification by 'nen_tang'.")
-        logging.info("✅ [ENRICH] Successfully standardized budget platform classification by 'nen_tang'.")
-    else:
-        print("⚠️ [ENRICH] Budget column 'nen_tang' not found then standardization is skipped.")
-        logging.warning("⚠️ [ENRICH] Budget column 'nen_tang' not found then standardization is skipped.")
-
-    # 2.1.2. Add composite key for join
-    required_cols = {"chuong_trinh", "noi_dung", "thang"}
-    print(f"🔍 [ENRICH] Adding budget composite key {required_cols}...")
-    logging.info(f"🔍 [ENRICH] Adding budget composite key {required_cols}...")
-    if required_cols.issubset(df.columns):
-        df["budget_key"] = (
-            df["chuong_trinh"].str.strip().str.upper() + "__" +
-            df["noi_dung"].str.strip().str.upper() + "__" +
-            df["thang"]
-        )
-        print(f"✅ [ENRICH] Successfully created composite key {required_cols}.")
-        logging.info(f"✅ [ENRICH] Successfully created composite key {required_cols}.")
-    else:
-        missing = required_cols - set(df.columns)
-        print(f"⚠️ [ENRICH] Missing columns for budget {missing}.")
-        logging.warning(f"⚠️ [ENRICH] Missing columns for budget {missing}.")
-
-    # 2.1.3. Enrich metadata from table_id
     try:
-        print(f"🔍 [ENRICH] Adding budget metadata from table_id...")
-        logging.info(f"🔍 [ENRICH] Adding budget metadata from table_id...")
-        table_name = table_id.split(".")[-1]
-        match = re.search(
-            r"^(?P<company>\w+)_table_budget_(?P<department>\w+)_(?P<account>\w+)_allocation_(?P<worksheet>\w+)$",
-            table_name
-        )
-        if match:
-            df["department"] = match.group("department")
-            df["account"] = match.group("account")
-            df["worksheet"] = match.group("worksheet")
-            print("✅ [ENRICH] Successfully added budget metadata from table_id.")
-            logging.info("✅ [ENRICH] Successfully added budget metadata from table_id.")
-    except Exception as e:
-        print(f"⚠️ [ENRICH] Failed to enrich budget metadata due to {e}.")
-        logging.warning(f"⚠️ [ENRICH] Failed to enrich budget metadata due to {e}.")
-    return df
+
+    # 1.1.3. Enrich table field(s) for staging Budget Allocation
+        enrich_section_name = "[ENRICH] Enrich table field(s) for staging Budget Allocation"
+        enrich_section_start = time.time()            
+        try: 
+            print(f"🔍 [ENRICH] Enriching table field(s) for staging Budget Allocation with {len(enrich_df_input)} row(s)...")
+            logging.info(f"🔍 [ENRICH] Enriching table field(s) for staging Budget Allocation with {len(enrich_df_input)} row(s)...")
+            enrich_df_table = enrich_df_input.copy()
+            enrich_df_table = enrich_df_table.assign(
+                spend=lambda df: pd.to_numeric(df["spend"], errors="coerce").fillna(0)            )
+            
+            enrich_table_name = enrich_table_id.split(".")[-1]
+            match = re.search(
+                r"^(?P<company>\w+)_table_(?P<platform>\w+)_(?P<department>\w+)_(?P<account>\w+)_allocation_m\d{6}$",
+                enrich_table_name
+            )            
+            enrich_df_table = enrich_df_table.assign(
+                enrich_account_platform=match.group("platform") if match else "unknown",
+                enrich_account_department=match.group("department") if match else "unknown",
+                enrich_account_name=match.group("account") if match else "unknown"
+            )            
+            print(f"✅ [ENRICH] Successfully enriched table field(s) for staging Budget Allocation with {len(enrich_df_table)} row(s).")
+            logging.info(f"✅ [ENRICH] Successfully enriched table field(s) for staging Budget Allocation with {len(enrich_df_table)} row(s).")
+            enrich_sections_status[enrich_section_name] = "succeed"        
+        except Exception as e:
+            enrich_sections_status[enrich_section_name] = "failed"
+            print(f"❌ [ENRICH] Failed to enrich table field(s) for staging TikTok Ads campaign insights due to {e}.")
+            logging.error(f"❌ [ENRICH] Failed to enrich table field(s) for staging TikTok Ads campaign insights due to {e}.")
+        finally:
+            enrich_sections_time[enrich_section_name] = round(time.time() - enrich_section_start, 2)
+
+    # 1.1.4. Enrich internal field(s) for staging Budget Allocation
+        enrich_section_name = "[ENRICH] Enrich internal field(s) for staging Budget Allocation"
+        enrich_section_start = time.time()            
+        try:
+            print(f"🔍 [ENRICH] Enriching internal field(s) for staging TikTok Ads campaign insights with {len(enrich_df_table)} row(s)...")
+            logging.info(f"🔍 [ENRICH] Enriching internal field(s) for staging TikTok Ads campaign insights with {len(enrich_df_table)} row(s)...")
+            enrich_df_internal = enrich_df_table.copy()
+            enrich_df_internal["nen_tang"] = enrich_df_internal["nen_tang"].astype(str).str.strip().str.lower()        
+            enrich_df_internal["nen_tang"] = enrich_df_internal["nen_tang"].astype(str).str.strip().str.lower()
+            enrich_df_internal["chuong_trinh"] = enrich_df_internal["chuong_trinh"].astype(str).str.strip().str.upper()
+            enrich_df_internal["noi_dung"] = enrich_df_internal["noi_dung"].astype(str).str.strip().str.upper()
+        except Exception as e:
+            enrich_sections_status[enrich_section_name] = "failed"
+            print(f"❌ [ENRICH] Failed to enrich table field(s) for staging TikTok Ads campaign insights due to {e}.")
+            logging.error(f"❌ [ENRICH] Failed to enrich table field(s) for staging TikTok Ads campaign insights due to {e}.")
+        finally:
+            enrich_sections_time[enrich_section_name] = round(time.time() - enrich_section_start, 2)
+
+    # 1.1.5. Enrich other field(s) for staging Budget Allocation
+        enrich_section_name = "[ENRICH] Enrich other field(s) for staging Budget Allocation"
+        enrich_section_start = time.time()            
+        try:
+            print(f"🔍 [ENRICH] Enriching other field(s) for staging Budget Allocation with {len(enrich_df_internal)} row(s)...")
+            logging.info(f"🔍 [ENRICH] Enriching other field(s) for staging Budget Allocation with {len(enrich_df_internal)} row(s)...")
+            enrich_df_other = enrich_df_internal.copy()
+            enrich_df_other = enrich_df_other.assign(
+                last_updated_at=lambda _: datetime.utcnow().replace(tzinfo=pytz.UTC),
+            )
+            print(f"✅ [ENRICH] Successfully enriched other field(s) for staging Budget Allocation with {len(enrich_df_other)} row(s).")
+            logging.info(f"✅ [ENRICH] Successfully enriched other field(s) for staging Budget Allocation with {len(enrich_df_other)} row(s).")
+            enrich_sections_status[enrich_section_name] = "succeed"
+        except Exception as e:
+            enrich_sections_status[enrich_section_name] = "failed"
+            print(f"❌ [ENRICH] Failed to enrich other field(s) for staging Budget Allocation due to {e}.")
+            logging.error(f"❌ [ENRICH] Failed to enrich other field(s) for staging Budget Allocation due to {e}.")
+        finally:
+            enrich_sections_time[enrich_section_name] = round(time.time() - enrich_section_start, 2) 
+
+    # 1.1.6. Summarize enrichment result(s) for staging Budget Allocation
+    finally:
+        enrich_time_elapsed = round(time.time() - enrich_time_start, 2)
+        enrich_df_final = enrich_df_other.copy() if not enrich_df_other.empty else pd.DataFrame()
+        enrich_sections_total = len(enrich_sections_status)
+        enrich_sections_failed = [k for k, v in enrich_sections_status.items() if v == "failed"]
+        enrich_sections_succeeded = [k for k, v in enrich_sections_status.items() if v == "succeed"]
+        enrich_rows_input = len(enrich_df_input)
+        enrich_rows_output = len(enrich_df_final)
+        enrich_sections_summary = list(dict.fromkeys(
+            list(enrich_sections_status.keys()) +
+            list(enrich_sections_time.keys())
+        ))
+        enrich_sections_detail = {
+            enrich_section_summary: {
+                "status": enrich_sections_status.get(enrich_section_summary, "unknown"),
+                "time": enrich_sections_time.get(enrich_section_summary, None),
+            }
+            for enrich_section_summary in enrich_sections_summary
+        }        
+        if any(v == "failed" for v in enrich_sections_status.values()):
+            print(f"❌ [ENRICH] Failed to complete staging Budget Allocation enrichment with {enrich_rows_output}/{enrich_rows_input} enriched row(s) due to section(s) {', '.join(enrich_sections_failed)} in {enrich_time_elapsed}s.")
+            logging.error(f"❌ [ENRICH] Failed to complete staging Budget Allocation enrichment with {enrich_rows_output}/{enrich_rows_input} enriched row(s) due to section(s) {', '.join(enrich_sections_failed)} in {enrich_time_elapsed}s.")
+            enrich_status_final = "enrich_failed_all"        
+        else:
+            print(f"🏆 [ENRICH] Successfully completed staging Budget Allocation enrichment with {enrich_rows_output}/{enrich_rows_input} enriched row(s) output in {enrich_time_elapsed}s.")
+            logging.info(f"🏆 [ENRICH] Successfully completed staging Budget Allocation enrichment with {enrich_rows_output}/{enrich_rows_input} enriched row(s) output in {enrich_time_elapsed}s.")
+            enrich_status_final = "enrich_succeed_all"                
+        enrich_results_final = {
+            "enrich_df_final": enrich_df_final,
+            "enrich_status_final": enrich_status_final,
+            "enrich_summary_final": {
+                "enrich_time_elapsed": enrich_time_elapsed,
+                "enrich_sections_total": enrich_sections_total,
+                "enrich_sections_succeed": enrich_sections_succeeded,
+                "enrich_sections_failed": enrich_sections_failed,
+                "enrich_sections_detail": enrich_sections_detail,
+                "enrich_rows_input": enrich_rows_input,
+                "enrich_rows_output": enrich_rows_output,
+            },
+        }
+    return enrich_results_final
